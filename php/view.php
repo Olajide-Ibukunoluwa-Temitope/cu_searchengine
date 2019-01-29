da21b95f998bbbc93d84f403a9830afa8eb26e9d
Olajide-Ibukunoluwa-Temitope <ibk12mails@gmail.com>
2019-01-29 06:53:22 +0000

    added bootstrap css

--- php/view.php
+++ php/view.php
@@ -29,6 +29,7 @@ $content = $result[2];
 	<title><?php echo $title ?></title>
 
 	<link rel="icon" href="../assets/images/favicon.ico">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="../assets/css/styles.min.css" rel="stylesheet">
 
