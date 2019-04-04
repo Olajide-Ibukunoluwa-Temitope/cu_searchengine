7db4106abde298943b2ce48d35232c76f6f26987
INOUE <laxusgooee@gmail.com>
2019-04-04 07:23:12 +0100

    bug fix

--- php/inc/search.inc.php
+++ php/inc/search.inc.php
@@ -95,16 +95,6 @@ count;
         return [$results, $queryTime, $allResults->num_rows];
     }
 
-
-    $now = date('Y-m-d h:i:s');
-
-    $insertQuery = <<<sql
-        INSERT INTO q_and_a(query, created_at, updated_at)
-        VALUES('$query', '$now', '$now');
-sql;
-
-    $conn->query($insertQuery);
-
     return null; // no result
 }
 
